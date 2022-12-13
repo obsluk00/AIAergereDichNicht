1391e5cfeee13278479277e746348ca877c0d8a2
luka.obser@unibas.ch <luka.obser@stud.unibas.ch>
2022-12-13 18:57:35 +0100

    pieces, movement of them and skeleton for both main game loop and legal move computation

--- game.py
+++ game.py
@@ -6,16 +6,74 @@ class Board:
     def __init__(self):
         self.board = [0] * 40
 
+    # piece moved from to
+    def moveFromTo(self, start, end):
+        if end >= 40:
+            end -= 40
+        if self.board[end] != 0:
+            self.board[end].knocked()
+        self.board[end] = self.board[start]
+        self.board[start].setSpace(end)
+        self.board[start] = 0
+
+    # player enters piece into play
+    def enterPiece(self, player, piece):
+        playerEntry = 10 * (player.color - 1) + 1
+        if self.board[playerEntry] != 0:
+            self.board[playerEntry].knocked()
+        self.board[playerEntry] = piece
+        piece.setSpace(playerEntry)
+
+    # player takes piece out of play
+    def leavePiece(self, piece, target):
+        self.board[piece.space] = 0
+        piece.setSpace(target)
+
+
+# pieces keep track on where on the board they are. if they havent entered the board they are on space 0,
+# if they have reached the safe spaces they are on -1, -2, -3 or -4
+class Piece:
+    def __init__(self):
+        self.space = 0
+    def setSpace(self, space):
+        self.space = space
+
+    # piece was knocked out
+    def knocked(self):
+        self.space = 0
+
 # color of player is encoded by enumeration from 1 through 4
 class Player:
     def __init__(self, color):
         self.color = color
-        self.baseCount = 4
+        self.pieces = [Piece(), Piece(), Piece(), Piece()]
         self.entryEmpty = True
-        self.home = [0, 0, 0, 0]
 
 def rollDice():
     return random.randrange(1, 7)
+
+# checks if player is finished
+def finishedCheck(player):
+    finished = True
+    for piece in player.pieces:
+        if piece.space >= 0:
+            finished = False
+    return finished
+
+# checks if player has a piece in the base
+def baseCheck(player):
+    for piece in player.pieces:
+        if piece.space == 0:
+            return True
+    return False
+
+# TODO: calculates legal moves, moves consist of piece moved and resulting board state
+def legalMoves(board, player, roll):
+    moves = []
+    for piece in player.pieces:
+        pass
+
+    return moves
 
 # determine who is the first to move. Yes this could just be done by drawing a random number
 def determineFirst():
@@ -39,3 +97,26 @@ def determineFirst():
             first = i
 
     return first
+
+def playGame():
+    players = [Player(1), Player(2), Player(3), Player(4)]
+    current = players[determineFirst() - 1]
+    board = Board()
+    while stillPlaying(players):
+        roll = rollDice()
+        moves = legalMoves(board, current, roll)
+        # TODO: chose which move to perform based on agents
+        chosenMove = None
+        board = chosenMove[1]
+        current = players[current.color % 4]
+
+def stillPlaying(players):
+    playingCount = 0
+    for player in players:
+        if not finishedCheck(player):
+            playingCount += 1
+    if playingCount > 1:
+        return True
+    else:
+        return False
+
